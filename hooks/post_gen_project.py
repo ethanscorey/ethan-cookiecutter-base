fbfd19d6ea23d5fc22b3ff7c74fbf86564f347e5
Ethan Corey <ethanscorey@gmail.com>
2022-11-11 19:20:16 -0500

    Added git init to post-install hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,4 +4,5 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run("pyenv install -s", shell=True, check=True)
     subprocess.run("./activate.sh", shell=True, check=True)
+    subprocess.run("git init .", shell=True, check=True)
     subprocess.run("pre-commit install", shell=True, check=True)
