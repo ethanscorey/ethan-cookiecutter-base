a846b7e03c40def041ff7a5d2d2283a7e07ada26
Ethan Corey <ethanscorey@gmail.com>
2022-11-11 23:09:15 -0500

    Hypermodernized templates

---
+++ {{ cookiecutter.project_slug }}/tests/test_installed.py
@@ -0,0 +1,7 @@
+"""Just test that we can import the package."""
+import {{ cookiecutter.project_slug }}
+
+
+def test_installed() -> None:
+    """Test that project can be imported."""
+    assert activecampaign_stripe_sync
