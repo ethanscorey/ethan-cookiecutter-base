44bacefb3aa508747749b60bcf85bb3f14a560fb
Ethan Corey <ethanscorey@gmail.com>
2022-11-11 19:19:26 -0500

    Added pre-commit installation to post-install hooks.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,3 +4,4 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run("pyenv install -s", shell=True, check=True)
     subprocess.run("./activate.sh", shell=True, check=True)
+    subprocess.run("pre-commit install", shell=True, check=True)
