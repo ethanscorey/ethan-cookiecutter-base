6d871c809d1a89cc91ed1bf881e39f511988d88d
Ethan Corey <ethanscorey@gmail.com>
2022-11-15 23:34:22 -0500

    Re-added nox

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     subprocess.run("./activate.sh", shell=True, check=True)
     subprocess.run("git init .", shell=True, check=True)
     subprocess.run("pre-commit install", shell=True, check=True)
+    subprocess.run("pre-commit run --all-files", shell=True, check=True)
---
+++ {{ cookiecutter.project_slug }}/noxfile.py
@@ -0,0 +1,48 @@
+from nox_poetry import session
+
+LOCATIONS = ("src", "tests", "noxfile.py")
+VERSIONS = ["{{ cookiecutter.python_version }}"]
+
+
+@session(python=VERSIONS)
+def tests(session):
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
+
+
+@session(python=VERSIONS)
+def lint(session):
+    args = session.posargs or LOCATIONS
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-isort",
+    )
+    session.run("flake8", *args)
+
+
+@session(python=VERSIONS)
+def safety(session):
+    session.install("safety")
+    session.run("safety", "check", "--file=pyproject.toml", "--full-report")
+
+
+{% if cookiecutter.mypy %}
+@session(python=VERSIONS)
+def mypy(session):
+    args = session.posargs or ["src"]
+    session.install(".")
+    session.install("mypy")
+    session.run("mypy", "--install-types")
+    session.run("mypy", *args)
+{% endif %}
+
+@session(python=VERSIONS)
+def format(session):
+    args = session.posargs or LOCATIONS
+    session.install("black", "isort")
+    session.run("black", *args)
+    session.run("isort", *args)
