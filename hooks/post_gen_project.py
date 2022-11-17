68ea753e96aa0b001f31aa54cbb57f729dfb4bdd
Ethan Corey <ethanscorey@gmail.com>
2022-11-17 16:53:21 -0500

    Removed pre-commit calls from hooks since they won't run unless pre-commit is installed in the environment that runs cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,3 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run("pyenv install -s", shell=True, check=True)
     subprocess.run("./activate.sh", shell=True, check=True)
-    subprocess.run("git init .", shell=True, check=True)
-    subprocess.run("pre-commit install", shell=True, check=True)
-    subprocess.run("pre-commit run --all-files", shell=True, check=True)
