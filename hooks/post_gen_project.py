c44f70a965c0e67ba756a84ee5e81913e4084a4e
Ethan Corey <ethanscorey@gmail.com>
2023-03-10 13:38:03 -0500

    Added Python version to pyenv call

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 if __name__ == "__main__":
-    subprocess.run("pyenv install -s", shell=True, check=True)
+    subprocess.run("pyenv install -s {{ cookiecutter.python_version }}", shell=True, check=True)
     subprocess.run("./activate.sh", shell=True, check=True)
