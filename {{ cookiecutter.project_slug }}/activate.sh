6916d305436972fc9026911ec4041e011f248d4b
Ethan Corey <ethanscorey@gmail.com>
2023-03-11 11:28:33 -0500

    Updated activate command.

--- {{ cookiecutter.project_slug }}/activate.sh
+++ {{ cookiecutter.project_slug }}/activate.sh
@@ -3,18 +3,12 @@
 # Determine the user's current shell
 SHELL_NAME=$(basename "$SHELL")
 
-# Set the appropriate activation command based on the shell
-if [ $SHELL_NAME = "bash" ]; then
-  ACTIVATE_CMD="bash"
-elif [ $SHELL_NAME = "zsh" ]; then
-  ACTIVATE_CMD="zsh"
+
+if [ "$SHELL_NAME" = "bash" ]; then
+  exec bash --rcfile .bashrc -i
+elif [ "$SHELL_NAME" = "zsh" ]; then
+  exec .source .zshrc
 else
   echo "Unsupported shell: $SHELL_NAME"
   exit 1
-fi
-
-if [ "$SHELL_NAME" = "bash" ]; then
-  exec "$ACTIVATE_CMD" --rcfile .bashrc
-elif [ "$SHELL_NAME" = "zsh" ]; then
-  exec "$ACTIVATE_CMD" --rcs .zshrc
 fi
