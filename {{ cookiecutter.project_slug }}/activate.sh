4a3bb078c6cedf6381a5fbfb53f321d95e53b998
Ethan Corey <ethanscorey@gmail.com>
2023-03-11 11:39:18 -0500

    Fixed activate.sh to work better with zsh

--- {{ cookiecutter.project_slug }}/activate.sh
+++ {{ cookiecutter.project_slug }}/activate.sh
@@ -7,7 +7,7 @@ SHELL_NAME=$(basename "$SHELL")
 if [ "$SHELL_NAME" = "bash" ]; then
   exec bash --rcfile .bashrc -i
 elif [ "$SHELL_NAME" = "zsh" ]; then
-  exec .source .zshrc
+  exec zsh -i -c 'source .zshrc && exec zsh'
 else
   echo "Unsupported shell: $SHELL_NAME"
   exit 1
